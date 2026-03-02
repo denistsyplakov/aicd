077485ef003db79ab85f44f113740f7410ee7897
denis.tsyplakov <denis.tsyplakov@gmail.com>
2026-03-02 12:13:05 +0400

    init

---
+++ src/main/java/com/github/denistsyplakov/aicd/AiCidingdemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.denistsyplakov.aicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCidingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCidingdemoApplication.class, args);
+	}
+
+}
